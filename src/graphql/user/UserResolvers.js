07e5d0d921c5b655348d28963f4e9c506a81376e
lXSPandora <luizepauloxd@gmail.com>
2018-06-30 17:15:14 -0300

    adds user add mutation

--- src/graphql/user/UserResolvers.js
+++ src/graphql/user/UserResolvers.js
@@ -1,5 +1,12 @@
 // @flow
 import UserModel from './UserModel';
+import type { UserType } from './UserTypes';
+
+type UserAdd = {
+  email: string,
+  name: string,
+  password: string,
+}
 
 const userResolvers = {
   me: () => ({
@@ -9,6 +16,25 @@ const userResolvers = {
     active: true,
   }),
   users: () => UserModel.find({}),
+  userAdd: async (obj: UserType, args: UserAdd) => {
+    const { email, name, password } = args;
+
+    if (!email || !name || !password) {
+      throw new Error('Please fill all the fields');
+    }
+
+    const user = new UserModel({
+      name,
+      email,
+      password,
+    });
+
+    await user.save();
+
+    const { _id } = user;
+
+    return UserModel.find({ _id });
+  },
 };
 
 export default userResolvers;
