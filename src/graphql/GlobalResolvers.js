fb8ff9c8aa030b6f497abc9f9c14aa30db8be562
lXSPandora <luizepauloxd@gmail.com>
2018-07-30 16:42:43 -0300

    ✨ adds encapsulated resolvers

--- src/graphql/GlobalResolvers.js
+++ src/graphql/GlobalResolvers.js
@@ -1,5 +1,6 @@
 // @flow
 import userResolvers from './user/UserResolvers';
+import postResolvers from './posts/PostResolvers';
 
 type ResolversType = {
   Query: Object,
@@ -11,11 +12,16 @@ const globalResolvers: ResolversType = {
     me: userResolvers.me,
     users: userResolvers.users,
     user: userResolvers.user,
+    post: postResolvers.post,
+    posts: postResolvers.posts,
   },
   Mutation: {
     userAdd: userResolvers.userAdd,
     login: userResolvers.login,
+    postAdd: postResolvers.postAdd,
   },
+  Post: postResolvers.Post,
+  User: userResolvers.User,
 };
 
 export default globalResolvers;
